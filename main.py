46ab13927bd12893e4e64d109f166afdddbb7051
Nisha091999 <118036003+Nisha091999@users.noreply.github.com>
2023-05-18 13:29:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Day1- String Manipulation")
+print('String cocatenation is done with the "+"sign')
+print('e.g. print("Hello"+"World")')
+print("New lines can be created with a backslash and n.")
